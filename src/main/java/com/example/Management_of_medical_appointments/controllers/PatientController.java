b835265b57d7849b0f385b2f37573b4711757cc7
Amós Judá <amosjudasilva@gmail.com>
2025-03-17 19:30:28 -0300

    getOne method created
    getOne method created and tested.

--- src/main/java/com/example/Management_of_medical_appointments/controllers/PatientController.java
+++ src/main/java/com/example/Management_of_medical_appointments/controllers/PatientController.java
@@ -8,12 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class PatientController {
@@ -30,5 +29,14 @@ public class PatientController {
     @GetMapping("/patient")
     public ResponseEntity<List<Patient>> getALLPatients() {
         return ResponseEntity.status(HttpStatus.OK).body(patientRepository.findAll());
+    }
+
+    @GetMapping("/patient/{id}")
+    public ResponseEntity<Object> getOnePatient(@PathVariable(value="id") UUID id) {
+        Optional<Patient> patientO = patientRepository.findById(id);
+        if(patientO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(patientO.get());
     }
 }
