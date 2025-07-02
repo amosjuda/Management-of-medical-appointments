1fdf3c4483fb5d6901a03ebcc3cac9f6e07dc6e0
amosjuda <amosjudasilva@gmail.com>
2025-07-02 15:09:25 -0300

    feat: starting with docker on database

--- target/generated-sources/annotations/com/amosjuda/Management_of_medical_appointments/config/DoctorMapperImpl.java
+++ target/generated-sources/annotations/com/amosjuda/Management_of_medical_appointments/config/DoctorMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-29T17:46:26-0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Microsoft)"
+    date = "2025-07-02T12:21:01-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Ubuntu)"
 )
 @Component
 public class DoctorMapperImpl implements DoctorMapper {
