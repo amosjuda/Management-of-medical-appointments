1b3c9f4a2b509b68dcac5178303d903642af4453
amosjuda <amosjudasilva@gmail.com>
2025-07-03 12:03:22 -0300

    feat: Containerize Spring Boot app with Docker, successfully containerized the medical appointments backend application using Docker. The project now runs within a Docker container, with the database also running in a separate Docker container, enabling a fully isolated and portable environment. API endpoints were successfully tested via Postman.

--- target/generated-sources/annotations/com/amosjuda/Management_of_medical_appointments/config/PatientMapperImpl.java
+++ target/generated-sources/annotations/com/amosjuda/Management_of_medical_appointments/config/PatientMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-02T12:21:01-0300",
+    date = "2025-07-03T11:42:16-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Ubuntu)"
 )
 @Component
